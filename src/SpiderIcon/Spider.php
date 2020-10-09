de4279adfa4d429e17b426818803f65e97fa29b9
duhuichun <duhuichun@baimaohui.net>
2020-10-09 15:47:09 +0800

    Spider the website's icon by url

---
+++ src/SpiderIcon/Spider.php
@@ -0,0 +1,80 @@
+<?php
+
+
+namespace SpiderIcon;
+
+
+use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
+
+class Spider
+{
+    /**
+     * 1、先判断url 是否能正常访问
+     * 2、如果能先获取url  link标签 rel属性包含"icon"的href
+     * href值可能类型
+     *      ../
+     *      /
+     *      完整url
+     *      图片base64
+     * 3、 href如果包含url 就去获取，如果不包含就拼接url
+     * @param string $url
+     * @return array
+     * @throws \GuzzleHttp\Exception\GuzzleException
+     * @author 1cool
+     * @date 2020/10/9 14:38
+     */
+    public function request(string $url): array
+    {
+        list($isValid, $realUrl) = Helper::checkUrlIsValid($url);
+
+        if (!$isValid) {
+            return [];
+        }
+
+        $crawler = new Crawler();
+
+        $client = new Client([
+            'verify'  => false,
+            'timeout' => 30,
+        ]);
+
+        try {
+            $crawler->addHtmlContent($client->get($realUrl)->getBody()->getContents());
+
+            $iconUrl = $crawler->filterXPath('//link[contains(@rel, "icon")]')->attr('href');
+        } catch (\Exception $exception) {
+            return [];
+        }
+
+        if (false !== strrpos($iconUrl, 'data:image/x-icon;base64')) {
+            // base64的icon
+            return [
+                'type'    => 'base64',
+                'content' => $iconUrl,
+            ];
+        }
+
+        if (false !== strrpos($iconUrl, 'http')) {
+            // 完整url
+            return [
+                'type'    => 'url',
+                'content' => $iconUrl,
+            ];
+        }
+
+        if (false !== strrpos($iconUrl, '../')) {
+            // 相对路径的图片
+            return [
+                'type'    => 'url',
+                'content' => $realUrl . str_replace('../', '/', $iconUrl),
+            ];
+        }
+
+        return [
+            'type'    => 'url',
+            'content' => $realUrl . $iconUrl,
+        ];
+    }
+
+}
